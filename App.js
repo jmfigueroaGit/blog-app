25235407294a89b438dfb2cbd7cdadb0900ddcfc
jmfigueroaGit <jheremiah.figueroa@tup.edu.ph>
2021-05-19 11:06:02 +0800

    Blog app using react native

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { LogBox } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import IndexScreen from './src/screens/IndexScreen';
+import { Provider } from './src/context/BlogContext';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+LogBox.ignoreLogs(['Remote debugger']);
+const Stack = createStackNavigator();
+
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='Index'>
+                <Stack.Screen
+                    name='Index'
+                    component={IndexScreen}
+                    options={{
+                        title: 'Blogs',
+                    }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default () => {
+    return (
+        <Provider>
+            <App />
+        </Provider>
+    );
+};
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,45 @@
+import React, { useContext } from 'react';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
+
+const IndexScreen = () => {
+    const { state, addBlogPosts } = useContext(Context);
+
+    return (
+        <View>
+            <Button title='Add Post' onPress={addBlogPosts} />
+            <FlatList
+                data={state}
+                keyExtractor={(blogPost) => blogPost.title}
+                renderItem={({ item }) => {
+                    return (
+                        <View style={styles.row}>
+                            <Text style={styles.title}>{item.title}</Text>
+                            <Feather name='trash' style={styles.icon} />
+                        </View>
+                    );
+                }}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: 'gray',
+    },
+    title: {
+        fontSize: 18,
+    },
+    icon: {
+        fontSize: 24,
+    },
+});
+
+export default IndexScreen;
